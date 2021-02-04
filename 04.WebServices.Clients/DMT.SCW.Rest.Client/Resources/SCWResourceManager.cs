ecf9d0fd6834926e4f1938459d6f559235fe198c
chumpon-asaneerat <chumponsenate@gmail.com>
2021-02-04 17:42:48 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #1327 - DMT.TAxTODv4   - Ported DMT.SCW.Rest.Client from DMT.TAxTODv3

---
+++ 04.WebServices.Clients/DMT.SCW.Rest.Client/Resources/SCWResourceManager.cs
@@ -0,0 +1,143 @@
+﻿#region Using
+
+using System;
+using System.IO;
+using System.Reflection;
+using NLib;
+
+#endregion
+
+namespace DMT.Services
+{
+    public class SCWResourceManager
+    {
+        #region Resouce Related Methods and Properties
+
+        private static Assembly Current { get { return typeof(SCWResourceManager).Assembly; } }
+
+        /// <summary>
+        /// Gets View SQL Script (from embedded resource).
+        /// </summary>
+        /// <param name="resourceName">The resource name.</param>
+        /// <returns>Returns load view sql script.</returns>
+        public static string GetScript(string resourceName)
+        {
+            MethodBase med = MethodBase.GetCurrentMethod();
+            string ret = string.Empty;
+            if (!string.IsNullOrWhiteSpace(resourceName))
+            {
+                try
+                {
+                    using (Stream stream = Current.GetManifestResourceStream(resourceName))
+                    {
+                        if (null != stream)
+                        {
+                            using (StreamReader reader = new StreamReader(stream))
+                            {
+                                if (null != reader)
+                                {
+                                    ret = reader.ReadToEnd();
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    med.Err(ex);
+                    ret = string.Empty;
+                }
+            }
+            return ret;
+        }
+        /// <summary>
+        /// Gets Json Resource Path.
+        /// </summary>
+        public static string JsonResourcePath { get { return "DMT.Resources.Json"; } }
+
+        private static string GetEmbeddedResourceName(string resourceName)
+        {
+            return JsonResourcePath + "." + resourceName + ".json";
+        }
+
+        private static string GetJson(string resourceName)
+        {
+            string script = string.Empty;
+            MethodBase med = MethodBase.GetCurrentMethod();
+            try
+            {
+                string embededResourceName = GetEmbeddedResourceName(resourceName);
+                script = GetScript(embededResourceName);
+            }
+            catch (Exception ex)
+            {
+                med.Err(ex);
+                script = string.Empty;
+            }
+            return script;
+        }
+
+        #endregion
+
+        #region public properties (static)
+
+        /// <summary>
+        /// Gets loginAudit parameter json script.
+        /// </summary>
+        public static string loginAudit { get { return GetJson("loginAudit"); } }
+        /// <summary>
+        /// Gets cardAllowList parameter json script.
+        /// </summary>
+        public static string cardAllowList { get { return GetJson("cardAllowList"); } }
+        /// <summary>
+        /// Gets couponBookList parameter json script.
+        /// </summary>
+        public static string couponBookList { get { return GetJson("couponBookList"); } }
+        /// <summary>
+        /// Gets couponList parameter json script.
+        /// </summary>
+        public static string couponList { get { return GetJson("couponList"); } }
+        /// <summary>
+        /// Gets currencyDenomList parameter json script.
+        /// </summary>
+        public static string currencyDenomList { get { return GetJson("currencyDenomList"); } }
+        /// <summary>
+        /// Gets jobList parameter json script.
+        /// </summary>
+        public static string jobList { get { return GetJson("jobList"); } }
+        /// <summary>
+        /// Gets jobList2 parameter json script.
+        /// </summary>
+        public static string jobList2 { get { return GetJson("jobList2"); } }
+        /// <summary>
+        /// Gets declare parameter json script.
+        /// </summary>
+        public static string declare { get { return GetJson("declare"); } }
+        /// <summary>
+        /// Gets emvTransactionList parameter json script.
+        /// </summary>
+        public static string emvTransactionList { get { return GetJson("emvTransactionList"); } }
+        /// <summary>
+        /// Gets qrcodeTransactionList parameter json script.
+        /// </summary>
+        public static string qrcodeTransactionList { get { return GetJson("qrcodeTransactionList"); } }
+        /// <summary>
+        /// Gets saveCheifDuty parameter json script.
+        /// </summary>
+        public static string saveCheifDuty { get { return GetJson("saveCheifDuty"); } }
+        /// <summary>
+        /// Gets cheifOnDuty parameter json script.
+        /// </summary>
+        public static string cheifOnDuty { get { return GetJson("cheifOnDuty"); } }
+        /// <summary>
+        /// Gets changePassword parameter json script.
+        /// </summary>
+        public static string changePassword { get { return GetJson("changePassword"); } }
+        /// <summary>
+        /// Gets passwordExpiresDays parameter json script.
+        /// </summary>
+        public static string passwordExpiresDays { get { return GetJson("passwordExpiresDays"); } }
+
+        #endregion
+    }
+}
---
+++ 04.WebServices.Clients/DMT.SCW.Rest.Client/Services/SCW.cs
@@ -0,0 +1,171 @@
+﻿#region Usings
+
+using System;
+using DMT.Models;
+using DMT.Configurations;
+
+#endregion
+
+namespace DMT.Services.Operations
+{
+    /// <summary>
+    /// The SCW Operation class.
+    /// </summary>
+    public static partial class SCW 
+    {
+        #region Static Methods
+
+        #region Get Client
+
+        /// <summary>
+        /// Get Client.
+        /// </summary>
+        /// <returns>Returns NRestClient instance.</returns>
+        public static NRestClient GetClient()
+        {
+            return NRestClient.CreateSCWClient(Config);
+        }
+
+        #endregion
+
+        #region Execute(s)
+
+        /// <summary>
+        /// Execute.
+        /// </summary>
+        /// <typeparam name="TResult">The Result type paramter.</typeparam>
+        /// <param name="url">The api url.</param>
+        /// <returns>Returns TResult instance.</returns>
+        public static TResult Execute<TResult>(string url)
+            where TResult : new()
+        {
+            TResult ret = default(TResult);
+            NRestClient client = GetClient();
+            if (null != client)
+            {
+                ret = client.Execute2<TResult>(url, new { }, Timeout, UserName, Password);
+            }
+            return ret;
+
+        }
+        /// <summary>
+        /// Execute.
+        /// </summary>
+        /// <typeparam name="TResult">The Result type paramter.</typeparam>
+        /// <param name="url">The api url.</param>
+        /// <param name="value">The parameter object.</param>
+        /// <returns>Returns TResult instance.</returns>
+        public static TResult Execute<TResult>(string url, object value)
+            where TResult : new()
+        {
+            TResult ret = default(TResult);
+            NRestClient client = GetClient();
+            if (null != client)
+            {
+                if (null != value)
+                    ret = client.Execute2<TResult>(url, value, Timeout, UserName, Password);
+                else ret = client.Execute2<TResult>(url, new { }, Timeout, UserName, Password);
+            }
+            return ret;
+
+        }
+        /// <summary>
+        /// Execute.
+        /// </summary>
+        /// <typeparam name="TResult">The Result type paramter.</typeparam>
+        /// <typeparam name="TValue">The Value type parameter.</typeparam>
+        /// <param name="url">The api url.</param>
+        /// <param name="value">The parameter object.</param>
+        /// <returns>Returns TResult instance.</returns>
+        public static TResult Execute<TResult, TValue>(string url, TValue value)
+            where TResult : new()
+        {
+            TResult ret = default(TResult);
+            NRestClient client = GetClient();
+            if (null != client && null != value)
+            {
+                ret = client.Execute2<TResult>(url, value, Timeout, UserName, Password);
+            }
+            return ret;
+
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Static Properties
+
+        /// <summary>
+        /// Gets or sets DMT config.
+        /// </summary>
+        public static IDMTConfig DMT { get; set; }
+        /// <summary>
+        /// Gets NetworkId.
+        /// </summary>
+        public static int NetworkId
+        {
+            get 
+            {
+                if (null == DMT) return 0;
+                if (null == DMT.DMT) return 0;
+                return DMT.DMT.networkId;
+            }
+        }
+        /// <summary>
+        /// Gets or sets service config.
+        /// </summary>
+        public static ISCWConfig Config { get; set; }
+        /// <summary>
+        /// Gets Base Address.
+        /// </summary>
+        public static string BaseAddress
+        {
+            get
+            {
+                if (null == Config) return string.Empty;
+                if (null == Config.SCW) return string.Empty;
+                if (null == Config.SCW.Service) return string.Empty;
+
+                return string.Format(@"{0}://{1}:{2}/",
+                    Config.SCW.Service.Protocol,
+                    Config.SCW.Service.HostName,
+                    Config.SCW.Service.PortNumber);
+            }
+        }
+        /// <summary>
+        /// Gets default execute timeout.
+        /// </summary>
+        public static int Timeout { get { return 1000; } }
+        /// <summary>
+        /// Gets user name.
+        /// </summary>
+        public static string UserName
+        {
+            get
+            {
+                if (null == Config) return string.Empty;
+                if (null == Config.SCW) return string.Empty;
+                if (null == Config.SCW.Service) return string.Empty;
+
+                return Config.SCW.Service.UserName;
+            }
+        }
+        /// <summary>
+        /// Gets password.
+        /// </summary>
+        public static string Password
+        {
+            get
+            {
+                if (null == Config) return string.Empty;
+                if (null == Config.SCW) return string.Empty;
+                if (null == Config.SCW.Service) return string.Empty;
+
+                return Config.SCW.Service.Password;
+            }
+        }
+
+        #endregion
+    }
+}
