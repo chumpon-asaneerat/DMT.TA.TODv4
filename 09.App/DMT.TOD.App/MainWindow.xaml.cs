c78937c509f3b5f67c6bcd1f46777f011922ba05
chumpon-asaneerat <chumponsenate@gmail.com>
2021-02-04 18:16:57 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #1338 - DMT.TAxTODv4   - Ported DMT.TOD.App from DMT.TAxTODv3

---
+++ 09.App/DMT.TOD.App/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿#region Using
+
+using System;
+using System.Windows;
+
+using NLib.Services;
+
+//using DMT.Models;
+using DMT.Services;
+
+#endregion
+
+namespace DMT
+{
+    using taOps = Services.Operations.TA.Notify;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Load/Unload
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Initial Page Content Manager
+            PageContentManager.Instance.ContentChanged += new EventHandler(Instance_ContentChanged);
+            PageContentManager.Instance.Start();
+        }
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+            // Release Page Content Manager
+            PageContentManager.Instance.Shutdown();
+            PageContentManager.Instance.ContentChanged -= new EventHandler(Instance_ContentChanged);
+        }
+
+        #endregion
+
+        #region Page Content Manager Handlers
+
+        void Instance_ContentChanged(object sender, EventArgs e)
+        {
+            this.container.Content = PageContentManager.Instance.Current;
+        }
+
+        #endregion
+    }
+}
