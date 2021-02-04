be56f1b1f1594926993e74b4f699afd544c19b23
chumpon-asaneerat <chumponsenate@gmail.com>
2021-02-04 18:06:47 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #1336 - DMT.TAxTODv4   - Ported DMT.Account.App from DMT.TAxTODv3

---
+++ 09.App/DMT.Account.App/Account/Pages/Menu/MainMenu.xaml.cs
@@ -0,0 +1,64 @@
+﻿#region Using
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+using NLib.Services;
+
+#endregion
+
+namespace DMT.Account.Pages.Menu
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Button Handlers
+
+        private void cmdCreditAndCouponSummary_Click(object sender, RoutedEventArgs e)
+        {
+            // TSB Credit and Coupon Summary
+        }
+
+        private void cndRequestExchangeHistory_Click(object sender, RoutedEventArgs e)
+        {
+            // TSB Request Exchange History
+        }
+
+        private void cmdTSBBalanceSummary_Click(object sender, RoutedEventArgs e)
+        {
+            // TSB Balance Summary
+        }
+
+        private void cndRequestExchangeManage_Click(object sender, RoutedEventArgs e)
+        {
+            // TSB Request Exchange Management
+        }
+
+        private void cndCouponSoldHistory_Click(object sender, RoutedEventArgs e)
+        {
+            // Coupon Sold History
+        }
+
+        private void cndExit_Click(object sender, RoutedEventArgs e)
+        {
+            // Exit
+        }
+
+        #endregion
+    }
+}
---
+++ 09.App/DMT.Account.App/App.xaml.cs
@@ -0,0 +1,132 @@
+﻿#region Using
+
+using System;
+using System.Windows;
+
+using NLib;
+using NLib.Logs;
+
+using DMT.Configurations;
+
+#endregion
+
+namespace DMT
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// OnStartup.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Console.WriteLine("OnStartUp");
+            if (null != AppDomain.CurrentDomain)
+            {
+                if (null != System.Threading.Thread.CurrentContext)
+                {
+                    Console.WriteLine("Thread CurrentContext is not null.");
+                }
+            }
+
+            #region Create Application Environment Options
+
+            EnvironmentOptions option = new EnvironmentOptions()
+            {
+                /* Setup Application Information */
+                AppInfo = new NAppInformation()
+                {
+                    /*  This property is required */
+                    CompanyName = "DMT",
+                    /*  This property is required */
+                    ProductName = AppConsts.Application.Account.ApplicationName,
+                    /* For Application Version */
+                    Version = AppConsts.Application.Account.Version,
+                    Minor = AppConsts.Application.Account.Minor,
+                    Build = AppConsts.Application.Account.Build,
+                    LastUpdate = AppConsts.Application.Account.LastUpdate
+                },
+                /* Setup Storage */
+                Storage = new NAppStorage()
+                {
+                    StorageType = NAppFolder.ProgramData
+                },
+                /* Setup Behaviors */
+                Behaviors = new NAppBehaviors()
+                {
+                    /* Set to true for allow only one instance of application can execute an runtime */
+                    IsSingleAppInstance = true,
+                    /* Set to true for enable Debuggers this value should always be true */
+                    EnableDebuggers = true
+                }
+            };
+
+            #endregion
+
+            #region Setup Option to Controller and check instance
+
+            WpfAppContoller.Instance.Setup(option);
+
+            if (option.Behaviors.IsSingleAppInstance &&
+                WpfAppContoller.Instance.HasMoreInstance)
+            {
+                return;
+            }
+
+            #endregion
+
+            #region Init Preload classes
+
+            ApplicationManager.Instance.Preload(() =>
+            {
+
+            });
+
+            #endregion
+
+            // Start log manager
+            LogManager.Instance.Start();
+
+            Window window = null;
+            window = new MainWindow();
+
+            if (null != window)
+            {
+                WpfAppContoller.Instance.Run(window);
+            }
+        }
+        /// <summary>
+        /// OnExit
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnExit(ExitEventArgs e)
+        {
+            // Shutdown log manager
+            LogManager.Instance.Shutdown();
+
+            // Wpf shutdown process required exit code.
+
+            /* If auto close the single instance must be true */
+            bool autoCloseProcess = true;
+            WpfAppContoller.Instance.Shutdown(autoCloseProcess, e.ApplicationExitCode);
+
+            base.OnExit(e);
+        }
+
+        private void Service_ConfigChanged(object sender, EventArgs e)
+        {
+            // When Service Config file changed.
+
+            // Update all related service operations.
+            Services.Operations.TAxTOD.Config = AccountConfigManager.Instance;
+            Services.Operations.TAxTOD.DMT = AccountConfigManager.Instance; // required for NetworkId
+            Services.Operations.SCW.Config = AccountConfigManager.Instance;
+            Services.Operations.SCW.DMT = AccountConfigManager.Instance; // required for NetworkId
+        }
+    }
+}
