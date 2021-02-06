813de15f8e0206b2a71c1e7c357d3676a4ca1c41
chumpon-asaneerat <chumponsenate@gmail.com>
2021-02-06 20:41:48 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #1386 - DMT.TAxTODv4   - DMT.Account.App project     - Refactor StatusBar controls and elements #1.

---
+++ 09.App/DMT.Account.App/StatusBar/Elements/AppInfoStatus.xaml.cs
@@ -0,0 +1,87 @@
+﻿#region Using
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+using NLib;
+
+using DMT.Configurations;
+using DMT.Services;
+
+#endregion
+
+namespace DMT.Controls.StatusBar
+{
+    /// <summary>
+    /// Interaction logic for AppInfoStatus.xaml
+    /// </summary>
+    public partial class AppInfoStatus : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public AppInfoStatus()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Loaded
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateUI();
+            /*
+            AccountConfigManager.Instance.ConfigChanged += ConfigChanged;
+            AccountUIConfigManager.Instance.ConfigChanged += UI_ConfigChanged;
+            */
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+            /*
+            AccountUIConfigManager.Instance.ConfigChanged -= UI_ConfigChanged;
+            AccountConfigManager.Instance.ConfigChanged -= ConfigChanged;
+            */
+        }
+
+        #endregion
+
+        #region Config Watcher Handlers
+
+        private void ConfigChanged(object sender, EventArgs e)
+        {
+            UpdateUI();
+        }
+
+        private void UI_ConfigChanged(object sender, EventArgs e)
+        {
+            UpdateUI();
+        }
+
+        #endregion
+
+        private void UpdateUI()
+        {
+            /*
+            var statusCfg = AccountUIConfigManager.Instance.AppInfo;
+            if (null == statusCfg || !statusCfg.Visible)
+            {
+                // Hide Control.
+                if (this.Visibility == Visibility.Visible) this.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                // Show Control.
+                if (this.Visibility != Visibility.Visible) this.Visibility = Visibility.Visible;
+            }
+            */
+
+            txtAppInfo.Text = ApplicationManager.Instance.Environments.Options.AppInfo.DisplayText;
+        }
+    }
+}
---
+++ 09.App/DMT.Account.App/StatusBar/Elements/TAServerStatus.xaml.cs
@@ -0,0 +1,193 @@
+﻿#region Using
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+using DMT.Configurations;
+using DMT.Services;
+
+#endregion
+
+namespace DMT.Controls.StatusBar
+{
+    //using wsOps = Services.Operations.TAxTOD.TAA;
+
+    /// <summary>
+    /// Interaction logic for TAServerStatus.xaml
+    /// </summary>
+    public partial class TAServerStatus : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public TAServerStatus()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        private DispatcherTimer timer = null;
+        private NLib.Components.PingManager ping = null;
+        private bool isOnline = false;
+
+        #region Loaded/Unloaded
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            /*
+            string host = (null != AccountConfigManager.Instance.TAxTOD && null != AccountConfigManager.Instance.TAxTOD.Service) ?
+                AccountConfigManager.Instance.TAxTOD.Service.HostName : "unknown";
+            int interval = (null != AccountUIConfigManager.Instance.TAServer) ?
+                AccountUIConfigManager.Instance.TAServer.IntervalSeconds : 5;
+            if (interval < 0) interval = 5;
+
+            ping = new NLib.Components.PingManager();
+            ping.OnReply += Ping_OnReply;
+            ping.Add(host);
+            ping.Interval = interval * 1000;
+            ping.Start();
+
+            CallWS();
+            UpdateUI();
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+
+            AccountConfigManager.Instance.ConfigChanged += ConfigChanged;
+            AccountUIConfigManager.Instance.ConfigChanged += UI_ConfigChanged;
+            */
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+            /*
+            AccountUIConfigManager.Instance.ConfigChanged -= UI_ConfigChanged;
+            AccountConfigManager.Instance.ConfigChanged -= ConfigChanged;
+
+            if (null != ping)
+            {
+                ping.OnReply -= Ping_OnReply;
+                ping.Stop();
+                ping.Dispose();
+            }
+            ping = null;
+
+            if (null != timer)
+            {
+                timer.Tick -= timer_Tick;
+                timer.Stop();
+            }
+            timer = null;
+            */
+        }
+
+        #endregion
+
+        #region Ping Reply Handler
+
+        private void Ping_OnReply(object sender, NLib.Networks.PingResponseEventArgs e)
+        {
+            if (null != e.Reply &&
+                e.Reply.Status == System.Net.NetworkInformation.IPStatus.Success)
+            {
+                // Call WS
+                CallWS();
+            }
+            else
+            {
+                isOnline = false;
+            }
+        }
+
+        #endregion
+
+        #region Timer Handler
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            UpdateUI();
+        }
+
+        #endregion
+
+        #region Config Watcher Handlers
+
+        private void ConfigChanged(object sender, EventArgs e)
+        {
+            /*
+            if (null != ping)
+            {
+                string host = (null != AccountConfigManager.Instance.TAxTOD && null != AccountConfigManager.Instance.TAxTOD.Service) ?
+                    AccountConfigManager.Instance.TAxTOD.Service.HostName : "unknown";
+                int interval = (null != AccountUIConfigManager.Instance.TAServer) ?
+                    AccountUIConfigManager.Instance.TAServer.IntervalSeconds : 5;
+                if (interval < 0) interval = 5;
+
+                // Stop ping service.
+                ping.Stop();
+                ping.Interval = interval * 1000;
+                // Clear and add new host.
+                ping.Clear();
+                ping.Add(host);
+                // Restart ping service.
+                ping.Start();
+            }
+            CallWS();
+            UpdateUI();
+            */
+        }
+
+        private void UI_ConfigChanged(object sender, EventArgs e)
+        {
+            CallWS();
+            UpdateUI();
+        }
+
+        #endregion
+
+        private void CallWS()
+        {
+            /*
+            var ret = wsOps.IsAlive();
+            isOnline = (ret.Ok) ? ret.Value().TimeStamp.HasValue : false;
+            //if (isOnline) Console.WriteLine(ret.Value().TimeStamp.Value.ToString("HH:mm:ss.fff"));
+            */
+        }
+
+        private void UpdateUI()
+        {
+            /*
+            var statusCfg = AccountUIConfigManager.Instance.TAServer;
+            if (null == statusCfg || !statusCfg.Visible)
+            {
+                // Hide Control.
+                if (this.Visibility == Visibility.Visible) this.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                // Show Control.
+                if (this.Visibility != Visibility.Visible) this.Visibility = Visibility.Visible;
+            }
+            */
+
+            if (isOnline)
+            {
+                borderStatus.Background = new SolidColorBrush(Colors.ForestGreen);
+                txtStatus.Text = "Online";
+            }
+            else
+            {
+                borderStatus.Background = new SolidColorBrush(Colors.Maroon);
+                txtStatus.Text = "Offline";
+            }
+        }
+    }
+}
