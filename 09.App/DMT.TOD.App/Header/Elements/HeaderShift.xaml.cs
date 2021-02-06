26cdf4418beb50d0602cc0d80b217a42912ccbc2
chumpon-asaneerat <chumponsenate@gmail.com>
2021-02-06 20:31:12 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #1383 - DMT.TAxTODv4   - DMT.TOD.App project     - Refactor Header controls and elements #1.

---
+++ 09.App/DMT.TOD.App/Header/Elements/HeaderShift.xaml.cs
@@ -0,0 +1,86 @@
+﻿#region Using
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+using DMT.Models;
+using DMT.Services;
+
+#endregion
+
+namespace DMT.Controls.Header
+{
+    /// <summary>
+    /// Interaction logic for HeaderShift.xaml
+    /// </summary>
+    public partial class HeaderShift : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public HeaderShift()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Loaded/Unloaded
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateUI();
+            /*
+            RuntimeManager.Instance.TSBChanged += Instance_TSBChanged;
+            RuntimeManager.Instance.TSBShiftChanged += Instance_TSBShiftChanged;
+            */
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+            /*
+            RuntimeManager.Instance.TSBShiftChanged -= Instance_TSBShiftChanged;
+            RuntimeManager.Instance.TSBChanged -= Instance_TSBChanged;
+            */
+        }
+
+        #endregion
+
+        #region RuntimeManager Handlers
+
+        private void Instance_TSBChanged(object sender, EventArgs e)
+        {
+            UpdateUI();
+        }
+
+        private void Instance_TSBShiftChanged(object sender, EventArgs e)
+        {
+            UpdateUI();
+        }
+
+        #endregion
+
+        private void UpdateUI()
+        {
+            /*
+            var shift = TSBShift.GetTSBShift().Value();
+            if (null != shift)
+            {
+                txtShiftDate.Text = shift.BeginDateString;
+                txtShiftTime.Text = shift.BeginTimeString;
+                txtShiftId.Text = shift.ShiftNameTH;
+            }
+            else
+            {
+                txtShiftDate.Text = string.Empty;
+                txtShiftTime.Text = string.Empty;
+                txtShiftId.Text = string.Empty;
+            }
+            */
+        }
+    }
+}
