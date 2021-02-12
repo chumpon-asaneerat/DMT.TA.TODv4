f5e4becfa188d4bf629abcfdfbe49d7d53cc55bc
chumpon-asaneerat <chumponsenate@gmail.com>
2021-02-12 16:55:16 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #1551 - DMT.TAxTODv4   - DMT.Core project.     - Ported Routes -> TA from v3.

---
+++ 01.Core/DMT.Core/Routes/TA/Shift/Actions/TSB/Change.cs
@@ -0,0 +1,23 @@
+﻿namespace DMT
+{
+    static partial class RouteConsts
+    {
+        static partial class TA
+        {
+            static partial class Shift
+            {
+                static partial class TSB
+                {
+                    /// <summary>The Gets Change TSB Shift action.</summary>
+                    public static class Change
+                    {
+                        /// <summary>Gets route name.</summary>
+                        public const string Name = "Change";
+                        /// <summary>Gets route url.</summary>
+                        public const string Url = TSB.Url + @"/" + Name;
+                    }
+                }
+            }
+        }
+    }
+}
