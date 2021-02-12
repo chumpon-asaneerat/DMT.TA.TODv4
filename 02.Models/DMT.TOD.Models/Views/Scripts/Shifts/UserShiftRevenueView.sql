501ad5f091375db420f95a9e15d8fb669631e9e4
chumpon-asaneerat <chumponsenate@gmail.com>
2021-02-12 17:33:22 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #1563 - DMT.TAxTODv4   - DMT.TOD.Models project.     - Ported from v3 and Add InitTSBAndPlazaAndLanes.     - Add Shift sql script files.

---
+++ 02.Models/DMT.TOD.Models/Views/Scripts/Shifts/UserShiftRevenueView.sql
@@ -0,0 +1,19 @@
+﻿CREATE VIEW UserShiftRevenueView
+AS
+	SELECT UserShiftRevenue.*
+		 , TSB.TSBNameEN, TSB.TSBNameTH
+		 , PlazaGroup.PlazaGroupNameEN, PlazaGroup.PlazaGroupNameTH, PlazaGroup.Direction
+		 , [Shift].ShiftNameEN, [Shift].ShiftNameTH
+	  FROM UserShiftRevenue
+		 , TSB
+	     , PlazaGroup
+		 , [Shift]
+		 , UserShift
+	 WHERE PlazaGroup.TSBId = TSB.TSBId
+	   AND UserShift.TSBId = TSB.TSBId
+	   AND UserShift.ShiftId = [Shift].ShiftId
+	   AND UserShiftRevenue.TSBId = TSB.TSBId
+	   AND UserShiftRevenue.PlazaGroupId = PlazaGroup.PlazaGroupId
+	   AND UserShiftRevenue.ShiftId = [Shift].ShiftId
+	   AND UserShiftRevenue.UserId = UserShift.UserId
+  ORDER BY UserShift.[Begin], [Shift].ShiftId, PlazaGroup.PlazaGroupId
---
+++ 02.Models/DMT.TOD.Models/Views/Scripts/Shifts/UserShiftView.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW UserShiftView
+AS
+	SELECT UserShift.*
+		 , TSB.TSBNameEN, TSB.TSBNameTH
+		 , [Shift].ShiftNameEN, [Shift].ShiftNameTH
+	  FROM UserShift
+		 , TSB
+	     , [Shift]
+	 WHERE UserShift.TSBId = TSB.TSBId
+	   AND UserShift.ShiftId = [Shift].ShiftId
+	 ORDER BY UserShift.[Begin], [Shift].ShiftId
