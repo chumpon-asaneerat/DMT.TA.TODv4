395b0010c94e36ac318121742068c6819ece3a53
chumpon-asaneerat <chumponsenate@gmail.com>
2021-02-12 17:28:48 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #1562 - DMT.TAxTODv4   - DMT.TA.Models project.     - Ported from v3 and Add InitTSBAndPlazaAndLanes.     - Add TSBShiftView.sql

---
+++ 02.Models/DMT.TA.Models/Views/Scripts/Shifts/TSBShiftView.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW TSBShiftView
+AS
+	SELECT TSBShift.*
+		 , TSB.TSBNameEN, TSB.TSBNameTH
+		 , [Shift].ShiftNameEN, [Shift].ShiftNameTH
+	  FROM TSBShift
+		 , TSB
+	     , [Shift]
+	 WHERE TSBShift.TSBId = TSB.TSBId
+	   AND TSBShift.ShiftId = [Shift].ShiftId
+	 ORDER BY TSBShift.[Begin], [Shift].ShiftId
