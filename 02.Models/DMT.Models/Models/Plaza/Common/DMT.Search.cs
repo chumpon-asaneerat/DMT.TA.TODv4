2c26c9453eb875a5348312d3804588369f0df0fc
chumpon-asaneerat <chumponsenate@gmail.com>
2021-02-17 02:58:10 +0700

    Refactor DMT.TA.TODv4 #1709 - DMT.TAxTODv4   - DMT.Models project.     - DMT.Search.cs -> added (exclude Revenue/Credit/Coupon).     - User model - added.

---
+++ 02.Models/DMT.Models/Models/Plaza/Common/DMT.Search.cs
@@ -0,0 +1,29 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+using SQLiteNetExtensions.Extensions;
+
+// required for JsonIgnore.
+using Newtonsoft.Json;
+using NLib;
+using NLib.Reflection;
+using System.Security.Permissions;
+
+#endregion
+
+namespace DMT.Models
+{
+    #region Search Main class (empty method).
+
+    /// <summary>
+    /// The Search class.
+    /// </summary>
+    public partial class Search { }
+
+    #endregion
+}
