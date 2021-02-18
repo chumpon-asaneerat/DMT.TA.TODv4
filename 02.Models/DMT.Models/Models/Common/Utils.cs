6dae9cbaac6b8afad657f27f84eb16e72c87adb1
chumpon-asaneerat <chumponsenate@gmail.com>
2021-02-19 06:20:49 +0700

    - DMT.TAxTODv4   - DMT.Core project.     - Ported WPF Utils from v3.   - DMT.Models project.     - Update MD5 Utils class from v3.

--- 02.Models/DMT.Models/Models/Common/Utils.cs
+++ 02.Models/DMT.Models/Models/Common/Utils.cs
@@ -9,51 +9,45 @@ using cypt = System.Security.Cryptography;
 
 #endregion
 
-namespace DMT.Models
+namespace DMT.Utils
 {
+    #region MD5
+
     /// <summary>
-    /// The DMT Utils class.
+    /// The MD5 Utils.
     /// </summary>
-    public static class Utils
+    public static class MD5
     {
-        #region MD5
-
         /// <summary>
-        /// The MD5 Utils.
+        /// Encrypt original string to MD5 string (UTF8).
         /// </summary>
-        public static class MD5
+        /// <param name="value">The original string.</param>
+        /// <returns>Returns encrypt string.</returns>
+        public static string Encrypt(string value)
         {
-            /// <summary>
-            /// Encrypt original string to MD5 string (UTF8).
-            /// </summary>
-            /// <param name="value">The original string.</param>
-            /// <returns>Returns encrypt string.</returns>
-            public static string Encrypt(string value)
-            {
-                return Encrypt(value, Encoding.UTF8);
-            }
-            /// <summary>
-            /// Encrypt original string to MD5 string.
-            /// </summary>
-            /// <param name="value">The original string.</param>
-            /// <param name="encoding">The Encoding instance. Default is UTF8.</param>
-            /// <returns>Returns encrypt string.</returns>
-            public static string Encrypt(string value, Encoding encoding)
-            {
-                string ret = string.Empty;
-
-                var encode = (null != encoding) ? encoding : Encoding.UTF8;
-                using (var md5 = cypt.MD5.Create())
-                {
-                    byte[] inputBytes = encode.GetBytes(value);
-                    byte[] hashBytes = md5.ComputeHash(inputBytes);
-                    ret = BitConverter.ToString(hashBytes)
-                        .Replace("-", string.Empty).ToLower();
-                }
-                return ret;
-            }
+            return Encrypt(value, Encoding.UTF8);
         }
+        /// <summary>
+        /// Encrypt original string to MD5 string.
+        /// </summary>
+        /// <param name="value">The original string.</param>
+        /// <param name="encoding">The Encoding instance. Default is UTF8.</param>
+        /// <returns>Returns encrypt string.</returns>
+        public static string Encrypt(string value, Encoding encoding)
+        {
+            string ret = string.Empty;
 
-        #endregion
+            var encode = (null != encoding) ? encoding : Encoding.UTF8;
+            using (var md5 = cypt.MD5.Create())
+            {
+                byte[] inputBytes = encode.GetBytes(value);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+                ret = BitConverter.ToString(hashBytes)
+                    .Replace("-", string.Empty).ToLower();
+            }
+            return ret;
+        }
     }
+
+    #endregion
 }
